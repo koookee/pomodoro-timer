a4646327ab85fdd6433c2779332702191f2241fc
koookee <55768904+koookee@users.noreply.github.com>
2021-01-11 17:54:16 -0500

    Trying to add the break UI elements.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import React from 'react';
 import Clock from './Components/Clock.js';
 
-function App() {
-  let display = {height: "100vh", backgroundImage: "linear-gradient(to top, #EDAFAA , #5B86E5)"}
-  return (
-    <div style={display}>
-    <Clock />
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+  }
+  render(){
+    let display = {height: "100vh", backgroundImage: "linear-gradient(to top, #EDAFAA , #5B86E5)"};
+    let breakDisplay = {position:"fixed",top:"50%", left:"30%", transform:"translate(-50%,-50%)",
+    fontSize:"2vw"};
+    let sessionDisplay = {};
+    return(
+      <div style={display}>
+        <Clock />
+        <p style={breakDisplay}>Break</p>
+        <button style={breakDisplay}>+</button>
+          <button style={breakDisplay}>-</button>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import ClockLogo from '../Logos/circle.png'
+import ClockLogo from '../Logos/circle.png';
+import '../CSS/Clock.css';
+import * as Icon from 'react-bootstrap-icons';
+
+
 class Clock extends React.Component{
   constructor(){
     super();
   }
   render(){
-    let clockDisplay = {position:"fixed",top:"50%", left:"50%", transform:"translate(-50%,-50%)"}
+    let clockDisplay = {
+      position:"fixed",top:"50%", left:"50%", transform:"translate(-50%,-50%)", width: "40vw",
+      height: "auto"
+    };
+    let timer = {
+      position:"fixed",top:"40%", left:"50%", transform:"translate(-50%,-50%)"
+    };
     return(
       <div>
-      <img style={clockDisplay} src={ClockLogo} alt="Clock Logo" />
+        <img style={clockDisplay} src={ClockLogo} alt="Clock Logo" />
+        <p style={timer} type="button">17:00</p>
+        <Icon.ArrowRight style={timer}> </Icon.ArrowRight>
       </div>
     )
   }
